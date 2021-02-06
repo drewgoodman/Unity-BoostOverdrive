dcd84964297ec1824b5c98d0a8fc213180ed3f14
rdogm13 <drg6525@yahoo.com>
2021-02-05 17:15:30 -0800

    sine wave oscillator movement for obstacles

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 8f;
 
     // TODO: remove from inspector later
     [Range(0,1)] [SerializeField] float movementFactor; // 0 not moved, 1 for fully moved.
@@ -22,6 +23,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
