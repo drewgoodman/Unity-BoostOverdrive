9cf2998a8cf99d2d8d3a4442916c57e0d7611cfe
rdogm13 <drg6525@yahoo.com>
2021-02-06 23:36:45 -0800

    added dynamic camera controller script

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cameraTarget;
+
+    Vector3 cameraTargetOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (cameraTarget)
+        {
+            cameraTargetOffset = transform.position - cameraTarget.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraTarget.transform.position + cameraTargetOffset;
+    }
+}
